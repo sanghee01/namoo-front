e327b41729967fb9eba1fbd9b357a0457b95c202
sararhdns7517 <sararhdns7517@naver.com>
2024-04-02 01:38:52 +0900

    feat: start 페이지 ui

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -3,38 +3,66 @@ import styled from "styled-components";
 const Start = () => {
   return (
     <Container>
-      <MainBox>
-        <p>
-          나만의 무럭이 키우기에 <br />
-          놀러오신 것을 환영합니다.
-        </p>
+      <Explain>
+        <p>나만의 무럭이 키우기에</p>
+        <p>놀러오신 것을 환영합니다!</p>
+      </Explain>
+      <SlideImg>
+        <LogoImg src="/assets/image/logoimg1.png"></LogoImg>
         <Logo src="/assets/image/logo2.png"></Logo>
+      </SlideImg>
+      <StartBtn>
         <button>시작하기</button>
-      </MainBox>
+      </StartBtn>
     </Container>
   );
 };
-
-const Logo = styled.img`
-  height: 40px;
-  top: 300px;
-  right: 27%;
-`;
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  padding: 50px 0;
+  justify-content: space-around;
+  height: 100%;
+  margin: auto;
 
   & p {
     font-weight: bold;
+    font-size: 20px;
     color: gray;
   }
 `;
+const Logo = styled.img`
+  width: 200px;
+`;
 
-const MainBox = styled.div`
-  position: relative;
+const LogoImg = styled.img`
+  width: 200px;
+  margin-bottom: 20px;
+`;
+
+const Explain = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 30px;
+`;
+
+const SlideImg = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StartBtn = styled.div`
+  & button {
+    padding: 15px 210px;
+    border-radius: 5px;
+    border: none;
+    font-weight: bold;
+    background-color: #8cd57e;
+  }
 `;
 export default Start;
