bd476d6707892b49aaae3efdc1502c69d0aa0cfb
sararhdns7517 <sararhdns7517@naver.com>
2024-04-02 00:17:01 +0900

    feat: 회원가입 ui 수정

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -11,7 +11,10 @@ const SignUp = () => {
       <Body>
         <InformationBox>
           <Input type="nickname" id="nickname" placeholder="닉네임 입력" />
-          <Input type="email" id="email" placeholder="이메일 입력" />
+          <EmailSend>
+            <Input type="email" id="email" placeholder="이메일 입력" />
+            <button>인증하기</button>
+          </EmailSend>
           <Input type="password" id="password" placeholder="비밀번호 입력" />
           <Input
             type="repassword"
@@ -29,7 +32,9 @@ const SignUp = () => {
             placeholder="키트를 배송 받을 배송지 입력"
           />
         </InformationBox>
-        <SignupBtn>가입하기</SignupBtn>
+        <SignupBtn>
+          <button>가입하기</button>
+        </SignupBtn>
       </Body>
     </Container>
   );
@@ -42,6 +47,7 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   padding: 30px 0;
+  height: 100%;
 `;
 
 const Header = styled.div`
@@ -67,9 +73,24 @@ const Body = styled.div`
 `;
 
 const InformationBox = styled.div`
-  width: 100%;
-  height: 50%;
-  justify-content: space-between;
+  display: flex;
+  flex-direction: column;
+`;
+
+const EmailSend = styled.div`
+  position: relative;
+  & button {
+    position: absolute;
+    width: 90px;
+    height: 40px;
+    border-radius: 5px;
+    border: none;
+    top: 13px;
+    right: 5px;
+    background-color: #8cd57e;
+    font-size: 13px;
+    font-weight: bold;
+  }
 `;
 
 const Input = styled.input`
@@ -81,16 +102,18 @@ const Input = styled.input`
   font-size: 13px;
 `;
 
-const SignupBtn = styled.button`
+const SignupBtn = styled.div`
+  display: flex;
   align-items: center;
   justify-content: center;
-  width: 70%;
-  margin-top: 30px;
-  margin-left: 15%;
-  padding: 10px;
-  border-radius: 5px;
-  border: none;
-  font-weight: bold;
-  background-color: #8cd57e;
+  margin-top: 50px;
+  & button {
+    width: 70%;
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+    font-weight: bold;
+    background-color: #8cd57e;
+  }
 `;
 export default SignUp;
--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,5 +1,40 @@
+import styled from "styled-components";
+
 const Start = () => {
-  return <>Start</>;
+  return (
+    <Container>
+      <MainBox>
+        <p>
+          나만의 무럭이 키우기에 <br />
+          놀러오신 것을 환영합니다.
+        </p>
+        <Logo src="/assets/image/logo2.png"></Logo>
+        <button>시작하기</button>
+      </MainBox>
+    </Container>
+  );
 };
 
+const Logo = styled.img`
+  height: 40px;
+  top: 300px;
+  right: 27%;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 50px 0;
+
+  & p {
+    font-weight: bold;
+    color: gray;
+  }
+`;
+
+const MainBox = styled.div`
+  position: relative;
+`;
 export default Start;
