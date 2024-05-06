7c488c7fa994a5027cff560b6b1adbcb350a0c31
이효진 <rabbit3456@naver.com>
2024-05-06 15:18:26 +0900

    fix: encyclopedia 반응형 수정

--- src/pages/Profile/Encyclopedia.tsx
+++ src/pages/Profile/Encyclopedia.tsx
@@ -1,9 +1,19 @@
 import styled from "styled-components";
+import { MdArrowBackIos } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { Text }from "./styles";
 
 const Encyclopedia = () => {
   return (
     <EncyclopediaBackGround>
-      <Header></Header>
+      <Header>
+        <Link to="/profile">
+          <MdArrowBackIos size="30"/>
+        </Link>
+          <Text>
+            식물 도감
+          </Text>
+      </Header>
     </EncyclopediaBackGround>
   );
 };
@@ -12,17 +22,16 @@ export const EncyclopediaBackGround = styled.div`
   background-image: url("/assets/images/bookshelf.png");
   background-repeat: no-repeat;
   background-color: #fffaed;
-  background-size: 550px;
+  background-size: 550px 700px;
   height: 100%;
 
   @media screen and (max-width: 600px){
-    background-size: 400px 600px;
+    background-size: 400px 650px;
   } 
 `;
 
 export const Header = styled.div`
   display: flex;
-  justify-content: space-between;
   align-items: center;
   padding: 25px;
   height: 10%;
