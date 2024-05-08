c6e70f7a366ecd03d725590a767f02c9630cc594
sanghee01 <sang6299@naver.com>
2024-05-08 15:45:45 +0900

    fix: changePasswordApi 매개변수 타입 null 지정

--- src/services/changePasswordApi.ts
+++ src/services/changePasswordApi.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-export async function changePassword(password: string, passwordConfirm: string, email: string, uuid: string) {
+export async function changePassword(
+  password: string,
+  passwordConfirm: string,
+  email: string | null,
+  uuid: string | null,
+) {
   const response = await axios.post(`${import.meta.env.VITE_SERVER_APIADDRESS}/member/password/change`, {
     password,
     passwordConfirm,
