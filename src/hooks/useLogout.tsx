a54444f5773f70c9eda0504c0dffca2d16d127bd
sanghee01 <sang6299@naver.com>
2024-05-13 15:56:27 +0900

    feat: 로그아웃 시 recoil 식물 변수 값 초기화

--- src/hooks/useLogout.tsx
+++ src/hooks/useLogout.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router";
 import { useRecoilState } from "recoil";
 import { userState } from "../state/userState";
 import { plantListState } from "../state/plantState";
+import { plantState } from "../state/plantState";
 
 const LOCAL_STORAGE_KEY = "userLocal";
 
@@ -14,12 +15,21 @@ const removeFromStorage = (key: string) => {
 const useLogout = () => {
   const navigate = useNavigate();
   const [, setUser] = useRecoilState(userState);
-  const [, setPlant] = useRecoilState(plantListState);
+  const [, setPlantList] = useRecoilState(plantListState);
+  const [, setPlant] = useRecoilState(plantState);
   const logout = () => {
     // 1. Recoil 상태 초기화
     setUser(null); // userState를 null로 설정하여 사용자 정보 초기화
-    setPlant([]); // 식물 정보도 초기화
-
+    setPlantList([]); // 식물 정보도 초기화
+    setPlant({
+      id: 0,
+      name: "",
+      exp: 0,
+      plantType: "",
+      uuid: "",
+      giveWater: false,
+      createDate: "",
+    });
     // 2. 로컬 스토리지에서 사용자 정보 제거
     removeFromStorage(LOCAL_STORAGE_KEY);
 
