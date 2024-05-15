881f3fb0421ea6c77be35e56b2b5cb4d84e09ecc
sararhdns7517 <sararhdns7517@naver.com>
2024-05-15 20:46:37 +0900

    feat: chat ui수정

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import styled from "styled-components";
+import { BsFillSendFill } from "react-icons/bs";
 
 const Chat = () => {
   const [messages, setMessages] = useState([{ id: 1, text: "안녕하세요! 무엇을 도와드릴까요?", isUser: false }]);
@@ -20,18 +21,25 @@ const Chat = () => {
     }
   };
 
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSendMessage();
+    }
+  };
+
   return (
     <ChatContainer>
       <Top></Top>
       <ChatHeader>
-        <RobotIcon></RobotIcon>
+        <RobotIcon>🤖</RobotIcon>
         <ChatTitle>Chatbot</ChatTitle>
       </ChatHeader>
       <ChatContent>
         {messages.map((message, index) => (
-          <ChatBubble key={index} isUser={message.isUser}>
-            {message.text}
-          </ChatBubble>
+          <BubbleWrapper key={index} isUser={message.isUser}>
+            {!message.isUser && <BubbleName>식물ai</BubbleName>}
+            <ChatBubble isUser={message.isUser}>{message.text}</ChatBubble>
+          </BubbleWrapper>
         ))}
       </ChatContent>
       <ChatInput>
@@ -39,8 +47,11 @@ const Chat = () => {
           placeholder="메시지를 입력하여 대화를 해주세요"
           value={inputText}
           onChange={(e) => setInputText(e.target.value)}
+          onKeyDown={handleKeyDown}
         />
-        <SendButton onClick={handleSendMessage}>전송</SendButton>
+        <SendButton onClick={handleSendMessage} style={{ fontSize: "auto" }}>
+          <BsFillSendFill size="75%" />
+        </SendButton>
       </ChatInput>
     </ChatContainer>
   );
@@ -56,29 +67,32 @@ const ChatContainer = styled.div`
 const Top = styled.div`
   height: 20px;
   width: 100%;
-  background-color: green;
+  background-color: #75c975;
 `;
 
 const ChatHeader = styled.div`
   display: flex;
   align-items: center;
   height: 10%;
-  padding: 5px 10px;
+  padding: 5px 15px;
   background-color: #ffffff;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
 `;
 
-const RobotIcon = styled.button``;
+const RobotIcon = styled.div`
+  font-size: 30px;
+  margin-right: 4px;
+`;
 
 const ChatTitle = styled.h3`
   flex-grow: 1;
-  font-size: 23px;
+  font-size: 25px;
   font-weight: bold;
 `;
 
 const ChatContent = styled.div`
   flex-grow: 1;
-  padding: 10px;
+  padding: 10px 15px;
   overflow-y: auto;
   margin-top: 10px;
 `;
@@ -90,8 +104,8 @@ interface ChatBubbleProps {
 const ChatBubble = styled.div<ChatBubbleProps>`
   background-color: ${(props) => (props.isUser ? "#61a263" : "#ececec")};
   color: ${(props) => (props.isUser ? "#ffffff" : "#333")};
-  padding: 10px;
-  font-size: 12px;
+  padding: 0.8rem;
+  font-size: 1rem;
   border-radius: 15px;
   margin-bottom: 12px;
   max-width: fit-content;
@@ -100,10 +114,26 @@ const ChatBubble = styled.div<ChatBubbleProps>`
   margin-right: ${(props) => (props.isUser ? "0" : "auto")};
 `;
 
+interface BubbleWrapper {
+  isUser: boolean;
+}
+const BubbleWrapper = styled.div<BubbleWrapper>`
+  display: flex;
+  flex-direction: column;
+  align-items: ${(props) => (props.isUser ? "flex-end" : "flex-start")};
+`;
+
+const BubbleName = styled.div`
+  font-weight: bold;
+  font-size: 17px;
+  margin-bottom: 0.2rem;
+  margin-left: 7px;
+`;
+
 const ChatInput = styled.div`
   display: flex;
   height: 8%;
-  padding: 1px;
+  padding: 2px 14px;
   background-color: #ffffff;
   box-shadow: 0 -1px 3px rgba(0, 0, 0, 0.1);
 `;
@@ -111,16 +141,14 @@ const ChatInput = styled.div`
 const InputBox = styled.input`
   flex-grow: 1;
   border: none;
-  padding: 0px 15px;
-  font-size: 15px;
+  font-size: 16px;
 `;
 
 const SendButton = styled.button`
-  background-color: #61a263;
-  color: #fff;
+  color: #939393;
+  background-color: transparent;
   border: none;
-  padding: 10px 30px;
-  border-radius: 20px;
+  padding-top: 5px;
   cursor: pointer;
 `;
 
