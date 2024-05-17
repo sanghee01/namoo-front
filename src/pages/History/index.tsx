1cd8a915f1d4bb1a00cfdbc80197981ac5a47ca2
sanghee01 <sang6299@naver.com>
2024-05-17 17:32:15 +0900

    feat: 히스토리 수직 막대 차트 구현

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
-import HistoryChart from "../../components/HistoryChart";
+import HistoryLineChart from "../../components/HistoryLineChart";
 import { useEffect } from "react";
 import { useGetPlantHistoryData } from "../../hooks/useGetPlantHistoryData";
 import { useRecoilValue } from "recoil";
 import { plantHistoryState } from "../../state/plantState";
+import HistoryBarChart from "../../components/HistoryBarChart";
 
 const History = () => {
   const getPlantHistoryData = useGetPlantHistoryData();
@@ -23,15 +24,15 @@ const History = () => {
   const dateList = plantHistoryData.map((data) => data.createdDate);
   const tempList = plantHistoryData.map((data) => data.temp);
   const humidityList = plantHistoryData.map((data) => data.humidity);
-  const soilHumidityList = plantHistoryData.map((data) => data.soilHumidity);
-  const lightList = plantHistoryData.map((data) => data.light);
-  const remainingWaterList = plantHistoryData.map((data) => data.remainingWater);
-  // const gaveWaterList = plantHistoryData.map((data) => data.gaveWater);
+  const soilHumidityList = plantHistoryData.map((data) => (data.soilHumidity / 1000) * 100);
+  const lightList = plantHistoryData.map((data) => (data.light / 3000) * 100);
+  const remainingWaterList = plantHistoryData.map((data) => (data.remainingWater / 3000) * 100);
+  const gaveWaterList = plantHistoryData.map((data) => Number(data.gaveWater));
 
   return (
     <Container>
       <h3>식물 히스토리</h3>
-      <HistoryChart
+      <HistoryLineChart
         dataName="온도(°C)"
         min={0}
         max={30}
@@ -40,7 +41,7 @@ const History = () => {
         borderColor="#ff7e5d"
         backgroundColor="#f1ad9c"
       />
-      <HistoryChart
+      <HistoryLineChart
         dataName="습도(%)"
         min={0}
         max={100}
@@ -49,42 +50,40 @@ const History = () => {
         borderColor="#8ebbf5"
         backgroundColor="#c0d9f5"
       />
-      <HistoryChart
-        dataName="토양 습도"
+      <HistoryLineChart
+        dataName="토양 습도(%)"
         min={0}
-        max={1000}
+        max={100}
         dataList={dateList}
         value={soilHumidityList}
         borderColor="#c27203"
         backgroundColor="#d7c4a7"
       />
-      <HistoryChart
-        dataName="조도"
+      <HistoryLineChart
+        dataName="빛의 세기(%)"
         min={0}
-        max={2000}
+        max={100}
         dataList={dateList}
         value={lightList}
         borderColor="#ffd000"
         backgroundColor="#fffed3"
       />
-      <HistoryChart
-        dataName="남은 물 양"
+      <HistoryBarChart
+        dataName="물통에 남은 물의 양(%)"
         min={0}
-        max={3000}
+        max={100}
         dataList={dateList}
         value={remainingWaterList}
-        borderColor="#1f75ff"
-        backgroundColor="#a4b3ff"
+        backgroundColor="#b9e8f7"
       />
-      {/* <HistoryChart
-        dataName="물주기 여부"
+      <HistoryBarChart
+        dataName="물주기 여부 (0, 1)"
         min={0}
         max={1}
         dataList={dateList}
         value={gaveWaterList}
-        borderColor="#37373b"
-        backgroundColor="#959595"
-      /> */}
+        backgroundColor="#979cf5"
+      />
     </Container>
   );
 };
@@ -94,7 +93,7 @@ export default History;
 const Container = styled.div`
   margin: 0 auto;
   width: 80%;
-  padding-bottom: 100px;
+  padding-bottom: 130px;
   h3 {
     padding: 30px 0;
   }
